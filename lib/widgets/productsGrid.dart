a850b14834131dd233b71b4ce28b78aad0f7e108
fardeen.iwa <fdcuf7392@gmail.com>
2022-12-05 22:37:05 +0500

    verbal push

--- lib/widgets/productsGrid.dart
+++ lib/widgets/productsGrid.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import '../providers/product_provider.dart';
+import '../providers/product.dart';
 
 import '../widgets/griditemTile.dart';
 
 import 'package:provider/provider.dart';
+//worke with other things as well as thes
 
 class productsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Product_provider>(context);
     final products = productsData.items;
+
     return GridView.builder(
       padding: const EdgeInsets.all(8),
-      itemBuilder: (ctx, index) => griditemTile(
-          products[index].id, products[index].title, products[index].imageUrl),
       itemCount: products.length,
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+          create: (ctx) => products[index],
+          // ignore: sort_child_properties_last
+          child: griditemTile(
+              //products[index].id, products[index].title, products[index].imageUrl),
+              )),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
